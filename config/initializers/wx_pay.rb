eae14aaea45431264df65904e5e0abca16a53d50
kevin xu <cming.xu@gmail.com>
2015-06-06 10:13:04 +0800

    MOD

--- config/initializers/wx_pay.rb
+++ config/initializers/wx_pay.rb
@@ -1,7 +1,7 @@
 # required
-WxPay.appid = 'wx97b3f3548f2c5c76'
+WxPay.appid = 'wx71753a63a0e5c2ec'
 WxPay.key = '184eac52da804e4542e0b2ef85b2c609'
-WxPay.mch_id = '1235113502'
+WxPay.mch_id = '1245742602'
 
 # optional - configurations for RestClient timeout, etc.
 WxPay.extra_rest_client_options = {timeout: 20, open_timeout: 23}
